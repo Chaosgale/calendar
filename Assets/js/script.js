c73de6fb03333c8abf69b999e78c65ad03e4fc79
Yan Ashe <yanashe@Yans-Mac-Studio.local>
2024-02-24 17:32:50 -0600

    final version of the application

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,63 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  const today = dayjs();
+  const dateFormat = today.format('dddd, MMMM D');
+  $('#currentDay').text(dateFormat);
+
+    function saveToLocalStorage(button) {
+      let textArea = $(button).siblings('.description');
+      let id = textArea.parent().attr('id'); 
+      let userInput = textArea.val().trim();
+ 
+      localStorage.setItem(id, userInput);
+      $('#appointmentDiv').css('visibility', 'visible')
+      appTimer()
+    }
+
+    function appTimer() {
+      let time = 3;
+      let answerIntervalId = setInterval(function () {
+        time--;
+        if (time <= 0) {
+            clearInterval(answerIntervalId);
+            $('#appointmentDiv').css('visibility', 'hidden')
+        }
+      }, 1000);
+    }
+  
+
+    $('.saveBtn').on('click', function() {
+      saveToLocalStorage(this);
+    });
+  
+  
+    function loadFromLocalStorage() {
+      $('.time-block').each(function() {
+        let id = $(this).attr('id'); 
+        let userInput = localStorage.getItem(id);
+  
+        if (userInput !== null) {
+          $(this).find('.description').val(userInput);
+        }
+      });
+    }
+  
+    loadFromLocalStorage();
+
+    let currentHour = dayjs().format('H');
+
+    $('[id^="hour-"]').each(function() {
+      let hourID = parseInt($(this).attr('id').split('-')[1]);
+  
+      if (currentHour > hourID) {
+        $(this).removeClass('present future').addClass('past');
+      } else if (currentHour == hourID) {
+        $(this).removeClass('past future').addClass('present');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
 });
